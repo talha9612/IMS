da6a43c4e40a55762e144d4f7fb12a5c3f4c79b1
Talha Saeed <talhasaeedgujjar444968@gmail.com>
2026-03-15 22:17:25 +0500

    Added Controller andd middlewares

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/testadmin',[AdminController::class, 'adminTest'])->middleware(['auth', 'verified','admin']);
 
-Route::get('/dashboard',[UserController::class, 'Dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[UserController::class, 'Dashboard'])->middleware(['admin', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
